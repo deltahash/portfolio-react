7e5ead851879694264316da6c982c09b275a5304
Rashek <Rashekhumayun@gmail.com>
2019-10-24 16:34:31 +0600

    initialiging react app

--- src/components/Navbar/Logo/Logo.js
+++ src/components/Navbar/Logo/Logo.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import Logo from "../../../assets/image/Logo.svg";
+import classes from "./Logo.module.css";
 
-import classes from './Logo.module.css';
-
-const logo = (props) => {
-  return <div className={classes.Logo}>Logo</div>;
+const logo = props => {
+  return (
+    <div className={classes.Logo}>
+      {" "}
+      <li className={classes.logo}>
+        <img src={Logo} alt="LOGO" />
+      </li>
+    </div>
+  );
 };
 
 export default logo;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,34 @@
-import React from 'react';
-
-import classes from './Navbar.module.css';
+import React from "react";
+import Menu from "../../assets/image/hamburger.svg";
+import classes from "./Navbar.module.css";
+import Logo from "./Logo/Logo";
 
 const navbar = () => {
-  return <div className={classes.Navbar}>Navbar</div>;
+  return (
+    <div className={classes.Navbar}>
+      {" "}
+      <nav>
+        <ul>
+          <Logo />
+          <li className="windowDisplay">
+            <a href="#home">TECHS</a>
+          </li>
+          <li className="windowDisplay">
+            <a href="#news">PROJECTS</a>
+          </li>
+          <li className="windowDisplay">
+            <a href="#contact">PORTFOLIO</a>
+          </li>
+          <li className="windowDisplay">
+            <a href="#about">FEEDBACK</a>
+          </li>
+          <li className="mobileDisplay">
+            <img src={Menu} alt="" />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default navbar;
